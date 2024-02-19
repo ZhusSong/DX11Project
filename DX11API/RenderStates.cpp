675c6a6c8417bc1af4816be66a33dae7facd2632
ZhusSong <42826951+ZhusSong@users.noreply.github.com>
2024-02-19 18:09:47 +0900

    24.2.19
    add renderState

---
+++ DX11API/RenderStates.cpp
@@ -0,0 +1,44 @@
+﻿#include "RenderStates.h"
+#include "DX11Utility.h"
+#include "DX11Debug.h"
+using namespace Microsoft:: WRL;
+
+ComPtr<ID3D11RasterizerState>RenderStates:: RSWireframe=nullptr;
+ComPtr<ID3D11RasterizerState>RenderStates::RSNoCull = nullptr;
+ComPtr<ID3D11RasterizerState>RenderStates::RSCullClockWise = nullptr;
+
+ComPtr<ID3D11SamplerState>RenderStates::SSLinearWrap = nullptr;
+ComPtr<ID3D11SamplerState>RenderStates::SSAnisotropicWrap = nullptr;
+
+ComPtr<ID3D11BlendState> RenderStates::BSAlphaToCoverage = nullptr;
+ComPtr<ID3D11BlendState>RenderStates::BSNoColorWrite = nullptr;
+ComPtr<ID3D11BlendState>RenderStates::BSTransparent = nullptr;
+ComPtr<ID3D11BlendState>RenderStates::BSAdditive = nullptr;
+
+ComPtr<ID3D11DepthStencilState>RenderStates::DSSWriteStencil = nullptr;
+ComPtr<ID3D11DepthStencilState>RenderStates::DSSDrawWithStencil = nullptr;
+ComPtr<ID3D11DepthStencilState>RenderStates::DSSNoDoubleBlend = nullptr;
+ComPtr<ID3D11DepthStencilState>RenderStates::DSSNoDepthTest = nullptr;
+ComPtr<ID3D11DepthStencilState>RenderStates::DSSNoDepthWrite = nullptr;
+ComPtr<ID3D11DepthStencilState>RenderStates::DSSNoDepthTestWithStencil = nullptr;
+ComPtr<ID3D11DepthStencilState>RenderStates::DSSNoDepthWriteWhithStencil = nullptr;
+
+bool RenderStates::IsInit()
+{
+	//判断是否已进行过初始化
+	return RSWireframe != nullptr;
+}
+
+void RenderStates::InitAll(ID3D11Device* device)
+{
+	if (IsInit())
+		return;
+	//初始化光栅化状态
+	D3D11_RASTERIZER_DESC rasterizerDesc;
+	ZeroMemory(&rasterizerDesc, sizeof(rasterizerDesc));
+
+	//线框模式
+
+
+
+}
---
+++ DX11API/RenderStates.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+//******************
+//提供一些渲染状态
+//******************
+#ifndef RENDERSTATES_H
+#define RENDERSTATES_H
+
+#include "WinAPISetting.h"
+#include <wrl/client.h>
+#include <d3d11_1.h>
+
+class RenderStates
+{
+	template <class T>
+	using ComPtr = Microsoft::WRL::ComPtr<T>;
+
+	static bool IsInit();
+
+	static void InitAll(ID3D11Device* device);
+
+public:
+	//光栅化状态:线框
+	static ComPtr<ID3D11RasterizerState> RSWireframe;
+	//光栅化状态:无背面裁剪
+	static ComPtr<ID3D11RasterizerState> RSNoCull;
+	//光栅化状态:顺时针裁剪
+	static ComPtr<ID3D11RasterizerState> RSCullClockWise;
+
+	//采样器状态:线性过滤
+	static ComPtr<ID3D11SamplerState> SSLinearWrap;
+	//采样器状态:各向异性过滤
+	static ComPtr<ID3D11SamplerState> SSAnisotropicWrap;
+
+	//混合状态:不写入颜色
+	static ComPtr<ID3D11BlendState> BSNoColorWrite;
+	//混合状态:透明混合
+	static ComPtr<ID3D11BlendState> BSTransparent;
+	//混合状态:Alpha-To-Coverage
+	static ComPtr<ID3D11BlendState> BSAlphaToCoverage;
+	//混合状态:加法混合
+	static ComPtr<ID3D11BlendState> BSAdditive;
+
+	//深度模板状态:写入模板值
+	static ComPtr<ID3D11DepthStencilState> DSSWriteStencil;
+	//深度模板状态:对指定模板值的区域进行绘制
+	static ComPtr<ID3D11DepthStencilState> DSSDrawWithStencil;
+	//深度模板状态:无二次混合区域
+	static ComPtr<ID3D11DepthStencilState> DSSNoDoubleBlend;
+	//深度模板状态:关闭深度测试
+	static ComPtr<ID3D11DepthStencilState> DSSNoDepthTest;
+	//深度模板状态:仅进行深度测试，不写入深度值
+	static ComPtr<ID3D11DepthStencilState> DSSNoDepthWrite;
+	//深度模板状态:关闭深度测试，对指定模板值的区域进行绘制
+	static ComPtr<ID3D11DepthStencilState> DSSNoDepthTestWithStencil;
+	//深度模板状态:仅进行深度测试，不写入深度值，对指定模板值的区域进行绘制
+	static ComPtr<ID3D11DepthStencilState> DSSNoDepthWriteWhithStencil;
+};
+
+#endif
